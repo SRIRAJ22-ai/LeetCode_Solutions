96072e621f8ddab29cf191b261b769a37b0bfe26
SRIRAJ22-ai <msaisriraj22@gmail.com>
2026-09-21 09:59:23 +0530

    Solved: Count Elements With Maximum Frequency [Easy]

---
+++ LeetCode/Easy/Count Elements With Maximum Frequency/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int freq[101] = {0};
+        for (int num : nums){
+            freq[num]++;
+        }
+        int maxFreq = 0;
+        for (int i = 1; i <= 100; i++){
+            maxFreq = max(maxFreq, freq[i]);
+        }
+        int result = 0;
+        for (int i = 1; i <= 100; i++){
+            if (freq[i] == maxFreq) {
+                result += freq[i];
+            }
+        }
+        return result;
+    }
+};
