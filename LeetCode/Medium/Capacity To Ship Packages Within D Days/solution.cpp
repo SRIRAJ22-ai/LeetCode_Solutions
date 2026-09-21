1cbf33200f978d61802789a526e0e9993f57069a
SRIRAJ22-ai <msaisriraj22@gmail.com>
2026-09-21 09:47:44 +0530

    Solved: Capacity To Ship Packages Within D Days [Medium]

---
+++ LeetCode/Medium/Capacity To Ship Packages Within D Days/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canShip(vector<int>& weights, int days, int capacity){
+        int usedDays = 1;
+        int currentWeight = 0;
+        for(int weight : weights){
+            if(currentWeight + weight > capacity){
+                usedDays++;
+                currentWeight = 0;
+            }
+            currentWeight += weight;
+        }
+        return usedDays <= days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = 0;
+        int high = 0;
+        for(int weight : weights){
+            low = max(low, weight);
+            high += weight;
+        }
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            if(canShip(weights, days, mid)){
+                high = mid;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
